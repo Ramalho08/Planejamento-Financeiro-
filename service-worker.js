f9de1e37ec8564ce2762606facddb81584dcdef6
Ramalho08 <guilhermeg240r@gmail.com>
2026-06-29 11:07:05 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='rf-v14-ultimate-modular-safe';
-const ASSETS=['./','./index.html','./style.css','./js/store.js','./js/ui.js','./js/actions.js','./js/app.js','./manifest.json','./assets/icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
