724e78734bcd20d66babb32349bda784ea4f2132
Ramalho08 <guilhermeg240r@gmail.com>
2026-06-29 15:02:57 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// R💰 Ramalho Finance V17 Cloud Prep
+// Este arquivo é seguro para GitHub Pages.
+// Na V18/V19 vamos preencher esses dados com o Firebase real.
+
+window.RF_FIREBASE_ENABLED = false;
+
+window.RF_FIREBASE_CONFIG = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
